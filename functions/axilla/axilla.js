e92a4d16b828f8febb4a55b0af8df1a14a6f4b5b
avidNewb <59672137+avidNewb@users.noreply.github.com>
2023-10-04 17:46:34 -0500

    testing

--- functions/axilla/axilla.js
+++ functions/axilla/axilla.js
@@ -72,7 +72,7 @@ exports.handler = async (event) => {
   // return the pixlet version when the `version` param is true
   if (isVersionRequest) {
 	var fs = require('fs')
-	var files = fs.readdirSync('./functions/axilla/')
+	var files = fs.readdirSync('./functions/axilla/assets/')
 	var filestring = files.join()
     try {
       const pixletVersion = await getPixletVersion()
